5a32e24a087b914fedc35c86ae875da4990e0038
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-02-15 21:03:50 -0300

    feat: add route to get a transaction

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -13,6 +13,18 @@ export async function transactionsRoutes(app: FastifyInstance) {
     return { transactions }
   })
 
+  app.get('/:id', async (request) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionsParamsSchema.parse(request.params)
+
+    const transactions = await knex('transactions').where('id', id).first()
+
+    return { transactions }
+  })
+
   app.post('/', async (request, replay) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),

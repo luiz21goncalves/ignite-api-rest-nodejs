4049d6fa7aa2cb6b1ecb0e40c9cf37032fb90a9a
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-02-15 21:03:08 -0300

    feat: add route to list transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -7,6 +7,12 @@ import { z } from 'zod'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select('*')
+
+    return { transactions }
+  })
+
   app.post('/', async (request, replay) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),

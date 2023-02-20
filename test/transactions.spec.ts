9a7273d32d94651b2ed26aee5a0070f5020698dc
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-02-20 14:15:41 -0300

    test: create a new transaction

---
+++ test/transactions.spec.ts
@@ -0,0 +1,31 @@
+import { execSync } from 'node:child_process'
+
+import request from 'supertest'
+import { expect, beforeAll, afterAll, describe, it, beforeEach } from 'vitest'
+
+import { app } from '../src/app'
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('pnpm run knex migrate:rollback --all')
+    execSync('pnpm run knex migrate:latest')
+  })
+
+  it('should be able to create a new transaction', async () => {
+    const response = await request(app.server).post('/transactions').send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})

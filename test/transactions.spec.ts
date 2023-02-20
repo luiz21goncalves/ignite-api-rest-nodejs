3ce031887d9bd40e961eb66b71206cb29ce963f3
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-02-20 14:39:37 -0300

    test: get the summary

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -38,6 +38,7 @@ describe('Transactions routes', () => {
         amount: 2500,
         type: 'credit',
       })
+      .expect(201)
 
     const cookies = createTransactionResponse.get('Set-Cookie')
 
@@ -64,6 +65,7 @@ describe('Transactions routes', () => {
         amount: 500,
         type: 'debit',
       })
+      .expect(201)
 
     const cookies = createTransactionResponse.get('Set-Cookie')
 
@@ -83,6 +85,38 @@ describe('Transactions routes', () => {
         title: 'New transaction',
         amount: '-500.00',
       }),
+    })
+  })
+
+  it('should be able to get the summary', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 2500,
+        type: 'credit',
+      })
+      .expect(201)
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({
+        title: 'New transaction',
+        amount: 1500,
+        type: 'debit',
+      })
+      .expect(201)
+
+    const summaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+
+    expect(summaryResponse.statusCode).toEqual(200)
+    expect(summaryResponse.body).toStrictEqual({
+      summary: { amount: '1000.00' },
     })
   })
 })
